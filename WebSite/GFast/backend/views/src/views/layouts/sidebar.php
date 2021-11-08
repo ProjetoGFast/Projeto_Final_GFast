fd68fcb8d2fffa99387408ae3cce72cb858266ee
duartefpereira <55538177+duartefpereira@users.noreply.github.com>
2021-11-08 11:10:07 +0000

    Backend upgrades

--- WebSite/GFast/backend/views/src/views/layouts/sidebar.php
+++ WebSite/GFast/backend/views/src/views/layouts/sidebar.php
@@ -16,9 +16,9 @@ AppAsset::register($this);
 
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-        <img src="<?=$assetDir?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+    <a href="index.php" class="brand-link">
+        <?= Html::img('@web/images/logo.png', ['class' => 'brand-image-xl'] ) ?>
+
     </a>
 
     <!-- Sidebar -->
@@ -26,6 +26,7 @@ AppAsset::register($this);
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
+
                 <img src="<?=$assetDir?>/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
@@ -35,7 +36,7 @@ AppAsset::register($this);
 
         <!-- SidebarSearch Form -->
         <!-- href be escaped -->
-        <!-- <div class="form-inline">
+         <div class="form-inline">
             <div class="input-group" data-widget="sidebar-search">
                 <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
                 <div class="input-group-append">
@@ -44,7 +45,7 @@ AppAsset::register($this);
                     </button>
                 </div>
             </div>
-        </div> -->
+        </div>
 
         <!-- Sidebar Menu -->
         <nav class="mt-2">
@@ -63,9 +64,14 @@ AppAsset::register($this);
                     ['label' => 'Simple Link', 'icon' => 'th', 'badge' => '<span class="right badge badge-danger">New</span>'],
                     ['label' => 'Yii2 PROVIDED', 'header' => true],
                     ['label' => 'Login', 'url' => ['site/login'], 'icon' => 'sign-in-alt', 'visible' => Yii::$app->user->isGuest],
-                    ['label' => 'Gii',  'icon' => 'file-code', 'url' => ['/gii'], 'target' => '_blank'],
-                    ['label' => 'Debug', 'icon' => 'bug', 'url' => ['/debug'], 'target' => '_blank'],
-                    ['label' => 'MULTI LEVEL EXAMPLE', 'header' => true],
+
+
+
+                    ['label' => 'Utilizadores',  'icon' => 'users', 'url' => ['user/index']],
+
+
+                    ['label' => 'Guitarras', 'icon' => 'bug', 'url' => ['guitarras/index']],
+                   /* ['label' => 'MULTI LEVEL EXAMPLE', 'header' => true],
                     ['label' => 'Level1'],
                     [
                         'label' => 'Level1',
@@ -83,11 +89,11 @@ AppAsset::register($this);
                             ['label' => 'Level2', 'iconStyle' => 'far']
                         ]
                     ],
-                    ['label' => 'Level1'],
+                   ['label' => 'Level1'],
                     ['label' => 'LABELS', 'header' => true],
                     ['label' => 'Important', 'iconStyle' => 'far', 'iconClassAdded' => 'text-danger'],
                     ['label' => 'Warning', 'iconClass' => 'nav-icon far fa-circle text-warning'],
-                    ['label' => 'Informational', 'iconStyle' => 'far', 'iconClassAdded' => 'text-info'],
+                    ['label' => 'Informational', 'iconStyle' => 'far', 'iconClassAdded' => 'text-info'],*/
                 ],
             ]);
             ?>
