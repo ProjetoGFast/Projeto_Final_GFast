de7a8179bd38fea270c5f4a955ab3ba0d00b8771
Alexandre Levchenko <2201155@my.ipleiria.pt>
2021-12-03 22:16:26 +0000

    Página das subcategorias

---
+++ WebSite/GFast/frontend/views/site/editarPerfil.php
@@ -0,0 +1,65 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\User */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="user-form" style="left:50%">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-3 col-6">
+            <?= $form->field($model, 'us_nome')->textInput(['maxlength' => true]) ?>
+
+
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-3 col-6">
+            <?= $form->field($model, 'us_apelido')->textInput(['maxlength' => true]) ?>
+        </div>
+
+    </div>
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+            <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+            <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+            <?= $form->field($model, 'us_cidade')->textInput(['maxlength' => true]) ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-3 col-6">
+            <?= $form->field($model, 'us_telemovel')->textInput() ?>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-3 col-6">
+            <?= $form->field($model, 'us_contribuinte')->textInput() ?>
+        </div>
+
+    </div>
+    <div class="row">
+        <div class="col-lg-2 col-md-2 col-sm-2 col-4">
+            <?= $form->field($model, 'us_pontos')->textInput() ?>
+        </div>
+    </div>
+
+
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
+
---
+++ WebSite/GFast/frontend/views/site/subcategoria.php
@@ -0,0 +1,14 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+
+?>
+
+<?= $this->render('..\layouts\header') ?>
+
+<?= $this->render('..\layouts\subcategorias') ?>
+
+<?= $this->render('..\layouts\footer') ?>
