e1c7d4ef5d7e37bfd16f274c6cd49a280ff2805b
Duarte Pereira <2190715@my.ipleiria.pt>
2021-12-21 23:03:36 +0000

    Android Updates

--- WebSite/GFast/backend/modules/v1/controllers/UserController.php
+++ WebSite/GFast/backend/modules/v1/controllers/UserController.php
@@ -2,6 +2,8 @@
 
 namespace backend\modules\v1\controllers;
 
+use common\models\LoginForm;
+use common\models\User;
 use yii\rest\ActiveController;
 use yii\web\Controller;
 
@@ -11,4 +13,30 @@ use yii\web\Controller;
 class UserController extends ActiveController
 {
   public $modelClass = 'common\models\User';
+
+    public function actionLogin()
+    {
+        $model = new LoginForm();
+
+        $model->username = \Yii::$app->request->post('username');
+        $model->password = \Yii::$app->request->post('password');
+        $modelUser = User::find()->where(['username' => $model->username])->one();
+
+        if ($modelUser->status != 10) {
+            throw new \yii\web\NotFoundHttpException("Username ou Password Incorretos!");
+
+        } else {
+
+            if($model->login())
+            {
+                return $modelUser;
+            }else
+            {
+                throw new \yii\web\NotFoundHttpException("Username ou Password Incorretos!");
+            }
+
+
+        }
+    }
+
 }
